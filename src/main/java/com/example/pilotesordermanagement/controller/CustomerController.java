3385144cc5e11c835ec5cde93b7fa519dbf04d07
danilo bruschetti <danilo.bruschetti@gmail.com>
2023-04-30 18:10:20 +0200

    refinement swagger doc

--- src/main/java/com/example/pilotesordermanagement/controller/CustomerController.java
+++ src/main/java/com/example/pilotesordermanagement/controller/CustomerController.java
@@ -2,37 +2,45 @@ package com.example.pilotesordermanagement.controller;
 
 import com.example.pilotesordermanagement.dto.CustomerDto;
 import com.example.pilotesordermanagement.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/V1/customers")
+@RequiredArgsConstructor
+@Tag(name = "Customers", description = "Customer management APIs")
 public class CustomerController {
 
   private final CustomerService customerService;
 
-  @Autowired
-  public CustomerController(CustomerService customerService) {
-    this.customerService = customerService;
-  }
-
   @PostMapping
+  @Operation(
+      summary = "Create a customer",
+      description = "Create a customer of Miquel Montoro's Resturant")
   public ResponseEntity<CustomerDto> createCustomer(@Valid @RequestBody CustomerDto customerDto) {
     CustomerDto createdCustomer = customerService.createCustomer(customerDto);
     return new ResponseEntity<>(createdCustomer, HttpStatus.CREATED);
   }
 
   @GetMapping("/{id}")
+  @Operation(
+      summary = "Find Customer By Id",
+      description = "Find a customer of Miquel Montoro's Resturant")
   public ResponseEntity<CustomerDto> getCustomerById(@PathVariable Long id) {
     CustomerDto customerDto = customerService.getCustomerById(id);
     return new ResponseEntity<>(customerDto, HttpStatus.OK);
   }
 
   @GetMapping
+  @Operation(
+      summary = "Find All Customers",
+      description = "Find all customer of Miquel Montoro's Resturant")
   public ResponseEntity<List<CustomerDto>> getAllCustomers() {
     List<CustomerDto> customers = customerService.getAllCustomers();
     return new ResponseEntity<>(customers, HttpStatus.OK);
--- src/main/java/com/example/pilotesordermanagement/controller/OrderController.java
+++ src/main/java/com/example/pilotesordermanagement/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.example.pilotesordermanagement.controller;
 
 import com.example.pilotesordermanagement.dto.OrderDto;
 import com.example.pilotesordermanagement.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -12,16 +14,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/V1/orders")
 @RequiredArgsConstructor
+@Tag(name = "Pilotes Order", description = "Order management APIs")
 public class OrderController {
   private final OrderService orderService;
 
   @PostMapping
+  @Operation(
+      summary = "Create an Order for a Customer",
+      description = "Create an Order for a Customer of Miquel Montoro's Resturant")
   public ResponseEntity<OrderDto> createOrder(@Valid @RequestBody OrderDto orderDto) {
     OrderDto createdOrder = orderService.createOrder(orderDto);
     return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
   }
 
   @PutMapping("/{id}")
+  @Operation(
+      summary = "Modify an Order for a Customer",
+      description = "Order cannot be modified after 5 minutes from creation")
   public ResponseEntity<OrderDto> updateOrder(
       @PathVariable Long id, @Valid @RequestBody OrderDto orderDto) {
     OrderDto updatedOrder = orderService.updateOrder(id, orderDto);
@@ -29,6 +38,9 @@ public class OrderController {
   }
 
   @GetMapping("/search")
+  @Operation(
+      summary = "Search All Orders",
+      description = "You can search all Orders only if you are ADMIN")
   public ResponseEntity<List<OrderDto>> searchOrdersByCustomerName(@RequestParam String query) {
     List<OrderDto> orders = orderService.searchOrdersByCustomerName(query);
     return new ResponseEntity<>(orders, HttpStatus.OK);
